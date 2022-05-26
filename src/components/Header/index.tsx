96a0a98a20c9a41d2800d32b68e587b6eb3d48e3
Cristian Camargo <chris.camargo2015@gmail.com>
2022-05-25 22:23:29 -0400

    Fix bug menu mobile open on click link, and fix scoll smooth

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,11 +10,6 @@ import { useState } from 'react';
 
 export function Header() {
 
-  const screenWidth = window.innerWidth
-  || document.documentElement.clientWidth
-  || document.body.clientWidth;
-  console.log(screenWidth)
-
   const [menuIsActive, setMenuIsActive] = useState(false);
 
   return (
@@ -31,15 +26,15 @@ export function Header() {
             <img src={CloseIcon} alt="Botao de fechar menu" />
           </div>
 
-          <li><NavLink to="#" title='Home'/></li>
-          <li><NavLink to="#about" title='Sobre'/></li>
-          <li><NavLink to="#skills" title='Skills'/></li>
-          <li><NavLink to="#portfoly" title='Portfolio'/></li>
-          <li><NavLink to="#contact" title='Contact'/></li>
+          <li onClick={()=> setMenuIsActive(false)}><NavLink to="#" title='Home'/></li>
+          <li onClick={()=> setMenuIsActive(false)}><NavLink to="#about" title='Sobre'/></li>
+          <li onClick={()=> setMenuIsActive(false)}><NavLink to="#skills" title='Skills'/></li>
+          <li onClick={()=> setMenuIsActive(false)}><NavLink to="#portfoly" title='Portfolio'/></li>
+          <li onClick={()=> setMenuIsActive(false)}><NavLink to="#contact" title='Contact'/></li>
         </ul>
 
-        <div className="toogle" onClick={()=>setMenuIsActive(true)}>
-          <img src={MenuButton} alt="Botao hamburguer para abrir menu" />
+        <div className="toogle" >
+          <img src={MenuButton} alt="Botao hamburguer para abrir menu" onClick={()=>setMenuIsActive(true)}/>
         </div>
     </HeaderNav>
   )
