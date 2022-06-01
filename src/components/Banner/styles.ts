c5a754dd382c93d35eb7dbca2db801c6f26c5379
Cristian Camargo <chris.camargo2015@gmail.com>
2022-06-01 09:03:50 -0400

    Fix responsives  bugs

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -81,7 +81,7 @@ export const ContentBanner = styled.div`
           gap:2rem;
           
           h1{
-            margin-top: 2rem;
+            margin-top: 4rem;
             font-size: 2.5rem;
             text-align: center;
             
