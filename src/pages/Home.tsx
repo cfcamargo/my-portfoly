85f615fb4f77b4f3590df316d9de7dfd410c1ec2
Cristian Camargo <chris.camargo2015@gmail.com>
2022-06-01 07:48:13 -0400

    Fix footer bugs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { BackToTop } from "../components/BackToTop";
 import { Banner } from "../components/Banner";
 import { Contact } from "../components/Contact";
 import { Footer } from "../components/Footer";
+
 import { Header } from "../components/Header";
 import { Portfoly } from "../components/Portfoly";
 import { Skills } from "../components/Skills";
